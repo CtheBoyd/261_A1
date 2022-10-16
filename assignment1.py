af96abb8c0e9e2f20d73b9e6ff21956264ca468e
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-10-16 11:54:32 -0700

    update

--- assignment1.py
+++ assignment1.py
@@ -37,7 +37,7 @@ def rotate(arr: StaticArray, steps: int) -> StaticArray:
     for i in range(0, steps):
         temp = arr[0]  # saving First element in temp variable
 
-            arr[j] = arr[j - 1]
+
         for j in range(0, arr.length() - 1):  # shift remaining array elements one by one
             arr[j] = arr[j + 1]
 
